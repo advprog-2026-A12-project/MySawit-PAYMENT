4623ea5cec62a94e8a7af173615daaeff1b7d33e
JaaTzy14 <mirzaradithya1410@gmail.com>
2026-02-20 15:18:16 +0700

    refactor: change import .* to more specific import

--- src/main/java/id/ac/ui/cs/advprog/mysawitpayment/controller/PayrollController.java
+++ src/main/java/id/ac/ui/cs/advprog/mysawitpayment/controller/PayrollController.java
@@ -4,7 +4,14 @@ import id.ac.ui.cs.advprog.mysawitpayment.dto.request.CreatePayrollRequest;
 import id.ac.ui.cs.advprog.mysawitpayment.dto.response.PayrollResponse;
 import id.ac.ui.cs.advprog.mysawitpayment.service.PayrollService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
