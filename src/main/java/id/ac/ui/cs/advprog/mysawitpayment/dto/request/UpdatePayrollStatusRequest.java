8882202351c10cc72d1df33fd3308a90ba5b355e
JaaTzy14 <mirzaradithya1410@gmail.com>
2026-02-20 14:53:54 +0700

    feat: add update payroll status request dto

---
+++ src/main/java/id/ac/ui/cs/advprog/mysawitpayment/dto/request/UpdatePayrollStatusRequest.java
@@ -0,0 +1,11 @@
+package id.ac.ui.cs.advprog.mysawitpayment.dto.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UpdatePayrollStatusRequest {
+
+    private String reason;
+}
