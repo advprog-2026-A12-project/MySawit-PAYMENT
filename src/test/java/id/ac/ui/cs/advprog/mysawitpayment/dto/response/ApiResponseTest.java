e5331d0fd865ecf815d0e167124b5cc727439f0a
JaaTzy14 <mirzaradithya1410@gmail.com>
2026-03-06 14:59:03 +0700

    test: create unit test for ApiResponse

---
+++ src/test/java/id/ac/ui/cs/advprog/mysawitpayment/dto/response/ApiResponseTest.java
@@ -0,0 +1,28 @@
+package id.ac.ui.cs.advprog.mysawitpayment.dto.response;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.OffsetDateTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ApiResponseTest {
+
+    @Test
+    void testBuilderAndGetter() {
+
+        OffsetDateTime now = OffsetDateTime.now();
+
+        ApiResponse<String> response = ApiResponse.<String>builder()
+                .status("success")
+                .message("Data retrieved")
+                .data("payload")
+                .timestamp(now)
+                .build();
+
+        assertEquals("success", response.getStatus());
+        assertEquals("Data retrieved", response.getMessage());
+        assertEquals("payload", response.getData());
+        assertEquals(now, response.getTimestamp());
+    }
+}
