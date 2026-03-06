817835b4ac06f92bcf1dcd61b0165efb09e3dea2
JaaTzy14 <mirzaradithya1410@gmail.com>
2026-03-06 15:28:03 +0700

    test: create unit test for MysawitPaymentApplication

--- src/test/java/id/ac/ui/cs/advprog/mysawitpayment/MysawitPaymentApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/mysawitpayment/MysawitPaymentApplicationTests.java
@@ -4,12 +4,16 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 @ActiveProfiles("test")
 class MysawitPaymentApplicationTests {
 
 	@Test
 	void contextLoads() {
+		MysawitPaymentApplication.main(new String[] {});
+		assertTrue(true, "Main method successfully executed");
 	}
 
 }
