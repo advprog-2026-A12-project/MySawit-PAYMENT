94867405cc81a9b42f7b70f1f0980da3d39a54a0
JaaTzy14 <mirzaradithya1410@gmail.com>
2026-03-06 11:59:17 +0700

    fix: fixed jwt secret

--- src/main/java/id/ac/ui/cs/advprog/mysawitpayment/security/JwtUtil.java
+++ src/main/java/id/ac/ui/cs/advprog/mysawitpayment/security/JwtUtil.java
@@ -13,7 +13,7 @@ public class JwtUtil {
 
     private final SecretKey signingKey;
 
-    public JwtUtil(@Value("${jwt.secret}") String secret) {
+    public JwtUtil(@Value("${JWT_SECRET}") String secret) {
         this.signingKey = Keys.hmacShaKeyFor(Decoders.BASE64.decode(secret));
     }
 
