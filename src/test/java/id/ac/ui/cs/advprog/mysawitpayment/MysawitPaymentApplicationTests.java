69d150818fce407992dcd6773bc3c269ae48694b
JaaTzy14 <mirzaradithya1410@gmail.com>
2026-03-06 16:08:14 +0700

    test: remove MysawitPaymentApplication test

--- src/test/java/id/ac/ui/cs/advprog/mysawitpayment/MysawitPaymentApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/mysawitpayment/MysawitPaymentApplicationTests.java
@@ -1,19 +1,12 @@
 package id.ac.ui.cs.advprog.mysawitpayment;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-@SpringBootTest
 @ActiveProfiles("test")
 class MysawitPaymentApplicationTests {
 
 	@Test
-	void contextLoads() {
-		MysawitPaymentApplication.main(new String[] {});
-		assertTrue(true, "Main method successfully executed");
-	}
+	void contextLoads() {}
 
 }
