5ba1d2972ea4313e5474b0fb8677d735727000db
JaaTzy14 <mirzaradithya1410@gmail.com>
2026-03-06 12:53:23 +0700

    refactor: removed unused imports

--- src/main/java/id/ac/ui/cs/advprog/mysawitpayment/service/PayrollService.java
+++ src/main/java/id/ac/ui/cs/advprog/mysawitpayment/service/PayrollService.java
@@ -2,12 +2,9 @@ package id.ac.ui.cs.advprog.mysawitpayment.service;
 
 import id.ac.ui.cs.advprog.mysawitpayment.dto.response.PayrollResponse;
 import id.ac.ui.cs.advprog.mysawitpayment.model.Payroll;
-import id.ac.ui.cs.advprog.mysawitpayment.model.enums.PayrollStatus;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
-import java.time.OffsetDateTime;
 import java.util.UUID;
 
 public interface PayrollService {
