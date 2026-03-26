34a3c464f16a15923d3f3f0425428007a7c204ed
JaaTzy14 <mirzaradithya1410@gmail.com>
2026-03-26 18:21:24 +0700

    test: create unit test for InsufficientBalanceException class

---
+++ src/test/java/id/ac/ui/cs/advprog/mysawitpayment/exception/InsufficientBalanceExceptionTest.java
@@ -0,0 +1,26 @@
+package id.ac.ui.cs.advprog.mysawitpayment.exception;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class InsufficientBalanceExceptionTest {
+
+    @Test
+    void testDefaultConstructor() {
+        InsufficientBalanceException exception =
+                new InsufficientBalanceException();
+
+        assertEquals("Insufficient wallet balance", exception.getMessage());
+    }
+
+    @Test
+    void testCustomMessageConstructor() {
+        String message = "Insufficient balance";
+
+        InsufficientBalanceException exception =
+                new InsufficientBalanceException(message);
+
+        assertEquals(message, exception.getMessage());
+    }
+}
