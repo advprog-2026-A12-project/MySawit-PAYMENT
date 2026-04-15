2a661877a3de212b63355627d68d4fca085774e6
JaaTzy14 <mirzaradithya1410@gmail.com>
2026-04-15 10:09:33 +0700

    feat: add more function to wallet service interface

--- src/main/java/id/ac/ui/cs/advprog/mysawitpayment/service/WalletService.java
+++ src/main/java/id/ac/ui/cs/advprog/mysawitpayment/service/WalletService.java
@@ -1,6 +1,11 @@
 package id.ac.ui.cs.advprog.mysawitpayment.service;
 
+import id.ac.ui.cs.advprog.mysawitpayment.dto.response.AdminWalletResponse;
+import id.ac.ui.cs.advprog.mysawitpayment.dto.response.WalletResponse;
+import id.ac.ui.cs.advprog.mysawitpayment.dto.response.WalletTransactionResponse;
 import id.ac.ui.cs.advprog.mysawitpayment.model.Wallet;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.math.BigDecimal;
 import java.util.UUID;
@@ -9,7 +14,11 @@ public interface WalletService {
 
     Wallet createWallet(UUID userId);
 
-    Wallet getWalletByUserId(UUID userId);
+    AdminWalletResponse getWalletByUserId(UUID userId);
+
+    WalletResponse getMyWallet(UUID userId);
+
+    Page<WalletTransactionResponse> getMyTransactions(UUID userId, Pageable pageable);
 
     void credit(UUID userId, BigDecimal amount, String referenceType, UUID referenceId);
 
