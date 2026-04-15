db6a6ee75276dc0d09a028b378e82bc57c139ae8
JaaTzy14 <mirzaradithya1410@gmail.com>
2026-04-15 10:19:21 +0700

    feat: remove unused function from wallet service interface

--- src/main/java/id/ac/ui/cs/advprog/mysawitpayment/service/WalletService.java
+++ src/main/java/id/ac/ui/cs/advprog/mysawitpayment/service/WalletService.java
@@ -3,24 +3,15 @@ package id.ac.ui.cs.advprog.mysawitpayment.service;
 import id.ac.ui.cs.advprog.mysawitpayment.dto.response.AdminWalletResponse;
 import id.ac.ui.cs.advprog.mysawitpayment.dto.response.WalletResponse;
 import id.ac.ui.cs.advprog.mysawitpayment.dto.response.WalletTransactionResponse;
-import id.ac.ui.cs.advprog.mysawitpayment.model.Wallet;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
-import java.math.BigDecimal;
 import java.util.UUID;
 
 public interface WalletService {
-
-    Wallet createWallet(UUID userId);
-
     AdminWalletResponse getWalletByUserId(UUID userId);
 
     WalletResponse getMyWallet(UUID userId);
 
     Page<WalletTransactionResponse> getMyTransactions(UUID userId, Pageable pageable);
-
-    void credit(UUID userId, BigDecimal amount, String referenceType, UUID referenceId);
-
-    void debit(UUID userId, BigDecimal amount, String referenceType, UUID referenceId);
 }
