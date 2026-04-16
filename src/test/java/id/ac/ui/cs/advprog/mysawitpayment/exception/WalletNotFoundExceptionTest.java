1b0c81c2ed3858b53ba08fb290c27015bd7ca3fc
JaaTzy14 <mirzaradithya1410@gmail.com>
2026-04-16 12:56:36 +0700

    test: create unit test for AdminWAlletResponse class

--- src/test/java/id/ac/ui/cs/advprog/mysawitpayment/exception/WalletNotFoundExceptionTest.java
+++ src/test/java/id/ac/ui/cs/advprog/mysawitpayment/exception/WalletNotFoundExceptionTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class WalletNotFoundExceptionTest {
+class WalletNotFoundExceptionTest {
 
     @Test
     void testDefaultConstructor() {
