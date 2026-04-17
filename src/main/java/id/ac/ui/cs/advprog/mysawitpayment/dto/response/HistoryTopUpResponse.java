ff2949f13b7770c8a910f0bf5afdfbc622ed4a37
JaaTzy14 <mirzaradithya1410@gmail.com>
2026-04-17 14:33:08 +0700

    feat: create HistoryTopUpResponse DTO

---
+++ src/main/java/id/ac/ui/cs/advprog/mysawitpayment/dto/response/HistoryTopUpResponse.java
@@ -0,0 +1,27 @@
+package id.ac.ui.cs.advprog.mysawitpayment.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Getter
+@Builder
+public class HistoryTopUpResponse {
+
+    private UUID id;
+
+    private BigDecimal amountSawitDollar;
+
+    private BigDecimal amountIdr;
+
+    private String paymentGateway;
+
+    private String status;
+
+    private OffsetDateTime createdAt;
+
+    private OffsetDateTime updatedAt;
+}
