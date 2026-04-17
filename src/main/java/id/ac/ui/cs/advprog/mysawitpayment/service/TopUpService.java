bb41f2c3384e267b7f26bcaafc039cb811b714cc
JaaTzy14 <mirzaradithya1410@gmail.com>
2026-04-17 17:45:08 +0700

    feat: create TopUpService interface

--- src/main/java/id/ac/ui/cs/advprog/mysawitpayment/service/TopUpService.java
+++ src/main/java/id/ac/ui/cs/advprog/mysawitpayment/service/TopUpService.java
@@ -1,5 +1,6 @@
 package id.ac.ui.cs.advprog.mysawitpayment.service;
 
+import id.ac.ui.cs.advprog.mysawitpayment.dto.XenditCallbackRequest;
 import id.ac.ui.cs.advprog.mysawitpayment.dto.request.CreateTopUpRequest;
 import id.ac.ui.cs.advprog.mysawitpayment.dto.response.CreateTopUpResponse;
 import id.ac.ui.cs.advprog.mysawitpayment.dto.response.HistoryTopUpResponse;
@@ -16,4 +17,6 @@ public interface TopUpService {
     Page<HistoryTopUpResponse> getMyTopUps(UUID adminId, Pageable pageable);
 
     TopUpDetailResponse getTopUpDetail(UUID id, UUID adminId);
+
+    void handleXenditCallback(String callbackToken, XenditCallbackRequest request);
 }
