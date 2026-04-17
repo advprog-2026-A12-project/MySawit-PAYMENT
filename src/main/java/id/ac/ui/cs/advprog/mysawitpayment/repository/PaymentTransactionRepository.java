65d086b5e3381ff63c15ff6d435001fbb5f1cf71
JaaTzy14 <mirzaradithya1410@gmail.com>
2026-04-17 15:13:13 +0700

    feat: add findByAdminId function to PaymentTransaction repository

--- src/main/java/id/ac/ui/cs/advprog/mysawitpayment/repository/PaymentTransactionRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/mysawitpayment/repository/PaymentTransactionRepository.java
@@ -1,6 +1,8 @@
 package id.ac.ui.cs.advprog.mysawitpayment.repository;
 
 import id.ac.ui.cs.advprog.mysawitpayment.model.PaymentTransaction;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -9,4 +11,6 @@ import java.util.UUID;
 public interface PaymentTransactionRepository extends JpaRepository<PaymentTransaction, UUID> {
 
     Optional<PaymentTransaction> findByGatewayReferenceId(String gatewayReferenceId);
+
+    Page<PaymentTransaction> findByAdminId(UUID adminId, Pageable pageable);
 }
