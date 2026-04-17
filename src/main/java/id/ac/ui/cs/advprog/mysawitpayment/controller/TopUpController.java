e03ac20092e8221e018aab1f8f3b41cd158f34ee
JaaTzy14 <mirzaradithya1410@gmail.com>
2026-04-17 18:55:00 +0700

    refactor: remove .* imports

--- src/main/java/id/ac/ui/cs/advprog/mysawitpayment/controller/TopUpController.java
+++ src/main/java/id/ac/ui/cs/advprog/mysawitpayment/controller/TopUpController.java
@@ -2,7 +2,11 @@ package id.ac.ui.cs.advprog.mysawitpayment.controller;
 
 import id.ac.ui.cs.advprog.mysawitpayment.dto.request.CreateTopUpRequest;
 import id.ac.ui.cs.advprog.mysawitpayment.dto.request.XenditCallbackRequest;
-import id.ac.ui.cs.advprog.mysawitpayment.dto.response.*;
+import id.ac.ui.cs.advprog.mysawitpayment.dto.response.ApiResponse;
+import id.ac.ui.cs.advprog.mysawitpayment.dto.response.TopUpDetailResponse;
+import id.ac.ui.cs.advprog.mysawitpayment.dto.response.HistoryTopUpResponse;
+import id.ac.ui.cs.advprog.mysawitpayment.dto.response.CreateTopUpResponse;
+import id.ac.ui.cs.advprog.mysawitpayment.dto.response.PageResponse;
 import id.ac.ui.cs.advprog.mysawitpayment.service.TopUpService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
@@ -10,7 +14,14 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 import java.time.OffsetDateTime;
 import java.util.Map;
