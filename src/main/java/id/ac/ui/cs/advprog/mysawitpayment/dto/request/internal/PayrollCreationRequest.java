d0e8871d2de0b988eb452edda55001485c08297e
JaaTzy14 <mirzaradithya1410@gmail.com>
2026-05-07 00:55:36 +0700

    feat: create PayrollCreationRequestDTO

---
+++ src/main/java/id/ac/ui/cs/advprog/mysawitpayment/dto/request/internal/PayrollCreationRequest.java
@@ -0,0 +1,29 @@
+package id.ac.ui.cs.advprog.mysawitpayment.dto.request.internal;
+
+import id.ac.ui.cs.advprog.mysawitpayment.model.enums.ReferenceType;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.Getter;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Getter
+public class PayrollCreationRequest {
+
+    @NotNull
+    private UUID userId;
+
+    @NotNull
+    private String userRole;
+
+    @NotNull
+    private ReferenceType referenceType;
+
+    @NotNull
+    private UUID referenceId;
+
+    @NotNull
+    @Positive
+    private BigDecimal kilogram;
+}
