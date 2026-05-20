6f39ecb5c3e16a77992a4d2d43a908c8636f0f7a
JaaTzy14 <mirzaradithya1410@gmail.com>
2026-05-20 23:40:52 +0700

    feat: make repository extends JpaSpecificationExecutor

--- src/main/java/id/ac/ui/cs/advprog/mysawitpayment/repository/PayrollRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/mysawitpayment/repository/PayrollRepository.java
@@ -1,33 +1,15 @@
 package id.ac.ui.cs.advprog.mysawitpayment.repository;
 
 import id.ac.ui.cs.advprog.mysawitpayment.model.Payroll;
-import id.ac.ui.cs.advprog.mysawitpayment.model.enums.PayrollStatus;
 import id.ac.ui.cs.advprog.mysawitpayment.model.enums.ReferenceType;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-import java.time.OffsetDateTime;
 import java.util.Optional;
 import java.util.UUID;
 
-public interface PayrollRepository extends JpaRepository<Payroll, UUID> {
-
-    Page<Payroll> findByUserId(UUID userId, Pageable pageable);
-
-    Page<Payroll> findByUserIdAndStatus(
-            UUID userId,
-            PayrollStatus status,
-            Pageable pageable
-    );
-
-    Page<Payroll> findByUserIdAndCreatedAtBetween(
-            UUID userId,
-            OffsetDateTime start,
-            OffsetDateTime end,
-            Pageable pageable
-    );
+public interface PayrollRepository extends JpaRepository<Payroll, UUID>, JpaSpecificationExecutor<Payroll> {
 
     Optional<Payroll> findByReferenceTypeAndReferenceIdAndUserId(
             ReferenceType referenceType,
--- src/main/java/id/ac/ui/cs/advprog/mysawitpayment/repository/WalletTransactionRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/mysawitpayment/repository/WalletTransactionRepository.java
@@ -1,14 +1,11 @@
 package id.ac.ui.cs.advprog.mysawitpayment.repository;
 
 import id.ac.ui.cs.advprog.mysawitpayment.model.WalletTransaction;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.UUID;
 
-public interface WalletTransactionRepository extends JpaRepository<WalletTransaction, UUID> {
-
-    Page<WalletTransaction> findByWalletId(UUID walletId, Pageable pageable);
-
+public interface WalletTransactionRepository
+        extends JpaRepository<WalletTransaction, UUID>, JpaSpecificationExecutor<WalletTransaction> {
 }
