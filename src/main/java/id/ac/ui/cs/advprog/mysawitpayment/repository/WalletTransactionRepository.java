67ede065a1a952101802a2a34796a2cfe6bc13a1
JaaTzy14 <mirzaradithya1410@gmail.com>
2026-05-21 15:36:46 +0700

    feat: create new query for wallet transaction

--- src/main/java/id/ac/ui/cs/advprog/mysawitpayment/repository/WalletTransactionRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/mysawitpayment/repository/WalletTransactionRepository.java
@@ -1,11 +1,19 @@
 package id.ac.ui.cs.advprog.mysawitpayment.repository;
 
 import id.ac.ui.cs.advprog.mysawitpayment.model.WalletTransaction;
+import id.ac.ui.cs.advprog.mysawitpayment.model.enums.TransactionType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface WalletTransactionRepository
         extends JpaRepository<WalletTransaction, UUID>, JpaSpecificationExecutor<WalletTransaction> {
+
+    Optional<WalletTransaction> findByReferenceTypeAndReferenceIdAndTransactionType(
+            String referenceType,
+            UUID referenceId,
+            TransactionType transactionType
+    );
 }
