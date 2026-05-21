656905139d1ed16cf82eeb651127461559bbc37e
JaaTzy14 <mirzaradithya1410@gmail.com>
2026-05-21 10:47:25 +0700

    feat: create find method with PESSIMISTIC_WRITE on payroll repository

--- src/main/java/id/ac/ui/cs/advprog/mysawitpayment/repository/PayrollRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/mysawitpayment/repository/PayrollRepository.java
@@ -3,8 +3,12 @@ package id.ac.ui.cs.advprog.mysawitpayment.repository;
 import id.ac.ui.cs.advprog.mysawitpayment.model.Payroll;
 import id.ac.ui.cs.advprog.mysawitpayment.model.enums.ReferenceType;
 
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 import java.util.UUID;
@@ -16,4 +20,8 @@ public interface PayrollRepository extends JpaRepository<Payroll, UUID>, JpaSpec
             UUID referenceId,
             UUID userId
     );
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("select payroll from Payroll payroll where payroll.id = :id")
+    Optional<Payroll> findByIdForUpdate(@Param("id") UUID id);
 }
