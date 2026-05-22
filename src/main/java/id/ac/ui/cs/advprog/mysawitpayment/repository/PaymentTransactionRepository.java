2615c115d1eb5fd8cfa83107bbbb675081124a48
JaaTzy14 <mirzaradithya1410@gmail.com>
2026-05-22 16:25:17 +0700

    feat: profiling

--- src/main/java/id/ac/ui/cs/advprog/mysawitpayment/repository/PaymentTransactionRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/mysawitpayment/repository/PaymentTransactionRepository.java
@@ -1,7 +1,10 @@
 package id.ac.ui.cs.advprog.mysawitpayment.repository;
 
 import id.ac.ui.cs.advprog.mysawitpayment.model.PaymentTransaction;
+import id.ac.ui.cs.advprog.mysawitpayment.model.enums.PaymentTransactionStatus;
 import jakarta.persistence.LockModeType;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Lock;
@@ -15,6 +18,14 @@ public interface PaymentTransactionRepository
         extends JpaRepository<PaymentTransaction, UUID>, JpaSpecificationExecutor<PaymentTransaction> {
 
     Optional<PaymentTransaction> findByGatewayReferenceId(String gatewayReferenceId);
+
+    Page<PaymentTransaction> findByAdminIdOrderByCreatedAtDesc(UUID adminId, Pageable pageable);
+
+    Page<PaymentTransaction> findByAdminIdAndStatusOrderByCreatedAtDesc(
+            UUID adminId,
+            PaymentTransactionStatus status,
+            Pageable pageable
+    );
 
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("select tx from PaymentTransaction tx where tx.id = :id")
